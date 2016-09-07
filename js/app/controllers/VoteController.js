d2e471d258d592a4807dcfaf7be541fbe61eb9ba
blessedsoy <unmigong@gmail.com>
2016-09-07 14:22:27 -0400

    Done.

--- js/app/controllers/VoteController.js
+++ js/app/controllers/VoteController.js
@@ -1,12 +1,31 @@
 function VoteController() {
-    this.votes = 0;
+    
+    this.votes = 5;
+    var myVote = 0
 
-    this.incrementVotes = function () {
+    this.incrementVotes = function () {    
+      if(myVote == 0){
+        this.votes++;
+        myVote = 1;
+      } else if (myVote == -1){
+        this.votes++;
+        myVote = 0;
+      } else {
+        this.decrementVotes();
+      }
 
     };
 
     this.decrementVotes = function () {
-
+      if(myVote == 0){
+        this.votes--;
+        myVote = -1;
+      } else if (myVote == 1){
+        this.votes--;
+        myVote = 0;
+      } else {
+        this.incrementVotes();
+      }
     };
 }
 
